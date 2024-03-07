10925a86e19baa90fac280673a67bea654bacd3a
Marco <mramog04@estudiantes.unileon.es>
2024-03-07 17:30:39 +0100

    Segimos trabajando en la practica 2

--- practica2/ED_pract2_2024/ED_Pract2_2024/src/ule/ed/list/ArrayEDList.java
+++ practica2/ED_pract2_2024/ED_Pract2_2024/src/ule/ed/list/ArrayEDList.java
@@ -20,7 +20,7 @@ public class ArrayEDList<T> implements IEDList<T> {
 		@Override
 		public boolean hasNext() {
 		//TODO
-		return false;
+		return current < count;
 		
 		}
 
@@ -28,7 +28,10 @@ public class ArrayEDList<T> implements IEDList<T> {
 		@Override
 		public T next() {
 			//TODO	
-			return null;
+			if(!hasNext()){
+				throw new NoSuchElementException();
+			}
+			return (T) data[current++];
 		}
 		}
 
@@ -80,7 +83,7 @@ public class ArrayEDList<T> implements IEDList<T> {
 	
 	private boolean contains(T elem){
 		boolean value = false;
-		for(int i = 0; i <= data.length;i++){
+		for(int i = 0; i < count;i++){
 			if(data[i].equals(elem)){
 				value = true;
 			}
@@ -164,13 +167,12 @@ public class ArrayEDList<T> implements IEDList<T> {
 	public T removeFirst() throws EmptyCollectionException {
 		// TODO 
 		emptyList();
-		T value;
-		T[] save = (T[])(new Object[data.length*2]);
-		for(int i = 0;i<=data.length;i++){
+		T value = data[0];
+		T[] save = (T[])(new Object[data.length]);
+		for(int i = 0;i<count;i++){
 			save[i-1]=data[i];
-			count--;
 		}
-		value=data[0];
+		count--;
 		data=save;
 		return value;
 		
@@ -180,9 +182,10 @@ public class ArrayEDList<T> implements IEDList<T> {
 	public T removelast() throws EmptyCollectionException {
 		// TODO 
 		emptyList();
-		T value;
+		T value = data[count-1];
+		count--;
 		value=data[count];
-		for(int i = 0;i<=data.length;i++){
+		for(int i = 0;i<count;i++){
 			data[i]=data[i+1];
 		}
 		return value;
@@ -190,32 +193,75 @@ public class ArrayEDList<T> implements IEDList<T> {
 
 	@Override
 	public T removePenult() throws EmptyCollectionException {
-		// TODO 
-		return null;
+		emptyList(); 
+		T value = data[count - 2];
+		T[] save = (T[]) (new Object[data.length]);
+		for (int i = 0; i < count - 1; i++) {
+			save[i] = data[i];
+		}
+		save[count - 1] = null;
+		data = save;
+		count--; 
+    return value;
 	}
+
 
 	@Override
 	public int removeElem(T elem) throws EmptyCollectionException {
 		// TODO 
-		return 0;	
+		emptyList();
+		int resultado=-1;
+		for(int i = 0;i<count;i++){
+			if(data[i].equals(elem)){
+				resultado=i;
+				break;
+			}
+		}
+		if (resultado==-1){
+			throw new NoSuchElementException("Elemento no econtrado");
+		}
+		T[] save = (T[])(new Object[data.length]);
+		for(int i=resultado;i<count;i++){
+			save[i]=data[i+1];
+		}
+		save[count-1]=null;
+		data=save;
+		count--;
+		return resultado;	
 	}
 	
 	@Override
 	public T getElemPos(int position) {
 		// TODO 
-		return null;
+		if(position<0||position>size()){
+			throw new IllegalArgumentException("Posicion invalida");
+		}
+		return data[position];
 	}
 
 	@Override
 	public int getPosLast(T elem) {
 		// TODO 
-		return 0;
+		elemnull(elem);
+		for(int i = data.length;i>=0;i--){
+			if (data[i]==elem){
+				return i;
+			}
+		}
+		throw new NoSuchElementException();
 	}
 
 	@Override
 	public int removeAll(T elem) throws EmptyCollectionException {
 		// TODO 
-		return 0;
+		int contador=0;
+		for(int i = 0;i<=data.length;i++){
+			if(data[i]==elem){
+				removeElem(elem);
+				contador++;
+			}
+		}
+		return contador;
 	}
 
 	@Override
