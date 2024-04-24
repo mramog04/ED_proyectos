9cf6c8d898ddd9b85c3bab8ebd024d897f18d8a6
Marco <mramog04@estudiantes.unileon.es>
2024-04-24 10:40:21 +0200

    Empezando la practica 4

---
+++ ED_pract4_2024/ED_Pract4_2024/src/ule/ed/recursivelist/LinkedEDList.java
@@ -0,0 +1,186 @@
+package ule.ed.recursivelist;
+
+
+public class LinkedEDList<T> implements EDList<T> {
+
+	//	referencia al primer  de la lista
+	private Node<T> front;
+
+	
+
+	private class Node<T> {
+
+		Node(T element) {
+			this.elem = element;
+			this.next = null;
+		}
+
+		T elem;
+
+		Node<T> next;
+	}
+
+	private void elemNull(T elem){
+		if(elem == null){
+			throw new NullPointerException();
+		}
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO 
+		return (this.front==null)? true:false;
+	}
+
+
+	@Override
+	public int size() {
+		// TODO RECURSIVAMENTE
+		return sizeRec(this.front);
+	}
+
+	private int sizeRec(Node<T> current){
+		if(current == null){
+			return 0;
+		}else{
+			return 1 + size(current.next);
+		}
+	}
+
+
+	@Override
+	public void addLast(T elem) {
+		// TODO RECURSIVAMENTE
+		elemNull(elem);
+		Node<T> newNode =  new Node<T>(elem);
+		addLastRec(this.front, newNode);
+	}
+
+	private Node<T> addLastRec(Node<T> current,Node<T> newNode){
+		if(size()==0){
+			current=newNode
+		}else if(current.next==null){
+			current.next=newNode
+		}else{
+			addLastRec(current.next, newNode);
+		}
+	}
+
+	
+	@Override
+	public void addPos(T elem, int position) {
+		// TODO RECURSIVAMENTE
+		
+	}
+
+	@Override
+	public boolean addBefore(T elem, T target) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public T getElemPos(int position) {
+		// TODO RECURSIVAMENTE
+		return null;
+	}
+
+
+
+	@Override
+	public int getPosFirst(T elem) {
+		// TODO RECURSIVAMENTE
+		return 0;
+	}
+
+
+
+	@Override
+	public int getPosLast(T elem) {
+		// TODO RECURSIVAMENTE
+		return 0;
+	}
+
+
+
+	@Override
+	public T removelast() throws EmptyCollectionException {
+		// TODO RECURSIVAMENTE
+		return null;
+	}
+
+
+
+	
+
+	@Override
+	public T removeLastElem(T elem) {
+		// TODO RECURSIVAMENTE
+		return null;
+	}
+
+
+
+	@Override
+	public EDList<T> reverse() {
+		// TODO RECURSIVAMENTE
+		return null;
+	}
+
+
+
+	@Override
+	public int removeOddElements(){
+		// TODO RECURSIVAMENTE
+		return 0;
+	}
+
+
+
+	@Override
+	public int removeConsecDuplicates() {
+		// TODO RECURSIVAMENTE
+		return 0;
+	}
+
+
+
+	@Override
+	public String toSringExceptFromUntilReverse(int from, int until) {
+		// TODO RECURSIVAMENTE
+		return null;
+	}
+
+
+	@Override
+	public boolean lengthEqualsTo(int n) {
+		// TODO RECURSIVAMENTE
+		return false;
+	}
+
+	@Override
+	public int removeEvenElements() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+	@Override
+	public int removeFirstElem(T elem) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String toString() {
+		// TODO RECURSIVAMENTE
+	
+		return null;
+	}
+
+
+	
+
+	
+	
+}
